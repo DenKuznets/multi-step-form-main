67a5dcee67e034dd79c9053b227abf93844f84e6
Denis Kuznetsov <denis.kyznecov@gmail.com>
2023-10-24 09:30:32 +0300

    redux added

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,11 +2,13 @@ import './globals.css';
 import type { Metadata } from 'next';
 import { raleway, merriweather, leagueSpartan } from './fonts';
 import React from 'react';
+import { Providers } from '../lib/providers';
+import Steps from '@/components/Steps/Steps';
 
 export const metadata: Metadata = {
     title: 'Frontend Mentor | Multi-step form',
     description:
-        'challenge designed by frontend-mentor, programmed by DenKuznets'
+        'challenge designed by frontend-mentor, made by DenKuznets'
 };
 
 export default function RootLayout({
@@ -15,11 +17,15 @@ export default function RootLayout({
     children: React.ReactNode;
 }) {
     return (
-        <html
-            className={`${raleway.variable} ${merriweather.variable} ${leagueSpartan.variable} font-serif`}
-            lang="ru"
-        >
-            <body>{children}</body>
-        </html>
+        <Providers>
+            <html
+                className={`${raleway.variable} ${merriweather.variable} ${leagueSpartan.variable} font-serif`}
+                lang="ru"
+            >
+                <body>
+                    <Steps>{children}</Steps>
+                </body>
+            </html>
+        </Providers>
     );
 }
