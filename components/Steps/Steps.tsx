61b37b79053e335da3b8eacb30005c3db04fef9b
Denis Kuznetsov <denis.kyznecov@gmail.com>
2023-10-25 15:13:53 +0300

    steps mobile bg set

--- components/Steps/Steps.tsx
+++ components/Steps/Steps.tsx
@@ -7,9 +7,15 @@ const Steps = (props: PropsWithChildren) => {
     const currentStep = useAppSelector(selectCurrentStep);
     const steps = useAppSelector(selectSteps);
     const stepsList = steps.map((step, index) => (
-        <div className="cursor-pointer" key={step.name}>
-            <div className="text-center rounded-full flex items-center justify-center h-8 w-8 border text-white">
-                {index}
+        <div className={`cursor-pointer `} key={step.name}>
+            <div
+                className={`${
+                    index + 1 === currentStep
+                        ? 'bg-lightBlue text-marineBlue'
+                        : 'bg-transparent text-white'
+                } flex h-8 w-8 items-center justify-center rounded-full border text-center font-bold`}
+            >
+                {index + 1}
             </div>
             <div className="hidden">Step {index}</div>
         </div>
@@ -17,9 +23,11 @@ const Steps = (props: PropsWithChildren) => {
     return (
         <div
             style={{ backgroundImage: 'url(./images/bg-sidebar-mobile.svg)' }}
-            className="pt-8 bg-no-repeat"
+            className="min-h-44 outline bg-[length:100%_100%] h-44 bg-no-repeat pt-8"
         >
-            <div className="flex flex-row justify-center gap-x-[1.1rem]">{stepsList}</div>
+            <div className="flex flex-row justify-center gap-x-[1.1rem]">
+                {stepsList}
+            </div>
             {props.children}
         </div>
     );
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -23,11 +23,11 @@ const config: Config = {
             body: ['var(--font-ubuntu)', 'sans-serif']
         },
         extend: {
-            backgroundImage: {
-                'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-                'gradient-conic':
-                    'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))'
-            },
+            // backgroundImage: {
+            //     'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+            //     'gradient-conic':
+            //         'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))'
+            // },
 
             colors: {
                 marineBlue: 'hsl(213, 96%, 18%)',
