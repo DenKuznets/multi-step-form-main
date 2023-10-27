94139305027a8745046feada4e0b4c71dce95af5
Denis Kuznetsov <denis.kyznecov@gmail.com>
2023-10-27 15:57:21 +0300

    1

--- app/[stepName]/layout.tsx
+++ app/[stepName]/layout.tsx
@@ -8,7 +8,7 @@ const layout = ({ params, children }: LayoutProps) => {
     return (
         <main className="relative flex min-h-screen flex-[1] flex-col items-center rounded-xl sm:m-auto sm:h-[37.5rem] sm:min-h-[unset] sm:max-w-[59rem] sm:flex-row sm:items-start sm:bg-white sm:p-4">
             <StepsList />
-            <div className="step relative top-[-5rem] mx-4 sm:pt-10 sm:px-20 sm:top-0 sm:h-full sm:w-full sm:p-0">
+            <div className="step relative top-[-5rem] mx-4 sm:mx-0 sm:pt-10 sm:px-20 sm:top-0 sm:h-full sm:w-full sm:p-0">
                 <h1 className="mb-2 text-[1.55rem] capitalize sm:mb-[0.9rem] sm:text-[2.05rem]">
                     {stepText?.header}
                 </h1>
