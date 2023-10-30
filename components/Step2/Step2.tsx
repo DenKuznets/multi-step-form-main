755bfba0965b84cd797f28fcc009e6bc3d848731
Denis Kuznetsov <denis.kyznecov@gmail.com>
2023-10-30 15:10:07 +0300

    added daisyui

--- components/Step2/Step2.tsx
+++ components/Step2/Step2.tsx
@@ -2,22 +2,32 @@ import PlanCard from './PlanCard/PlanCard';
 
 const Step2 = () => {
     return (
-        <div className='flex flex-col gap-4'>
-            <PlanCard
-                imgUrl="./images/icon-arcade.svg"
-                planName="Arcade"
-                planInfo="$9/mo"
-            />
-            <PlanCard
-                imgUrl="./images/icon-advanced.svg"
-                planName="Advanced"
-                planInfo="$12/mo"
-            />
-            <PlanCard
-                imgUrl="./images/icon-pro.svg"
-                planName="Pro"
-                planInfo="$15/mo"
-            />
+        <div>
+            <div className="flex flex-col gap-4">
+                <PlanCard
+                    imgUrl="./images/icon-arcade.svg"
+                    planName="Arcade"
+                    planInfo="$9/mo"
+                />
+                <PlanCard
+                    imgUrl="./images/icon-advanced.svg"
+                    planName="Advanced"
+                    planInfo="$12/mo"
+                />
+                <PlanCard
+                    imgUrl="./images/icon-pro.svg"
+                    planName="Pro"
+                    planInfo="$15/mo"
+                />
+            </div>
+            <div className="rounded-md bg-alabaster ">
+                <div className="form-control">
+                    <label className="label cursor-pointer">
+                        <span className="label-text">Remember me</span>
+                        <input type="checkbox" className="toggle" checked />
+                    </label>
+                </div>
+            </div>
         </div>
     );
 };
