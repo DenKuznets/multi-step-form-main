b3ae7ec9df8456efaf0b186341665fe7bf46befb
Denis Kuznetsov <denis.kyznecov@gmail.com>
2023-10-30 11:47:04 +0300

    step2 added

--- app/[stepName]/page.tsx
+++ app/[stepName]/page.tsx
@@ -2,15 +2,18 @@
 
 import { PageProps } from '@/.next/types/app/page';
 import Step1 from '@/components/Step1/Step1';
+import Step2 from '@/components/Step2/Step2';
 
 const page = ({ params }: PageProps) => {
     switch (params.stepName) {
         case 'personal-info':
             return <Step1 />;
+        case 'select-plan':
+            return <Step2 />;
 
-            default:
-                break;
-            }
+        default:
+            break;
+    }
 };
 
 export default page;
