da11b331301e1245bd4baabf1348e03efeb4d0c7
Denis Kuznetsov <denis.kyznecov@gmail.com>
2023-11-02 11:48:38 +0300

    mo yr step4

--- components/Step4/Step4.tsx
+++ components/Step4/Step4.tsx
@@ -5,15 +5,20 @@ import Link from 'next/link';
 
 const Step4 = () => {
     const router = useRouter();
+    // const [monthly, setMonthly] = useState(false);
+    const monthly = false;
+    // const [yearly, setYearly] = useState(false);
+    const multiplier = monthly ? 1 : 10;
+    const period = monthly ? 'mo' : 'yr';
 
     return (
         <form
             noValidate
             aria-label="select add-ons form"
-            className="text-sm lg:text-base text-coolGray [&_*]:first-letter:capitalize"
+            className="text-sm text-coolGray lg:text-base [&_*]:first-letter:capitalize"
             // onSubmit={handleSubmit(onSubmit)}
         >
-            <div className="bg-alabaster p-4 mb-6">
+            <div className="mb-6 bg-alabaster p-4">
                 <div className="flex items-center justify-between">
                     <span>
                         <span className="font-bold capitalize text-marineBlue">
@@ -27,22 +32,24 @@ const Step4 = () => {
                             change
                         </Link>
                     </span>
-                    <span className="font-bold text-marineBlue">$9/mo</span>
+                    <span className="font-bold text-marineBlue">{`$${
+                        9 * multiplier
+                    }/${period}`}</span>
                 </div>
                 <hr className="my-3" />
-                <div className="flex justify-between mb-3">
+                <div className="mb-3 flex justify-between">
                     <span>online service</span>
-                    <span>+$1/mo</span>
+                    <span>{`+$${1 * multiplier}/${period}`}</span>
                 </div>
                 <div className="flex justify-between">
                     <span>larger storage</span>
-                    <span>+$2/mo</span>
+                    <span>{`+$${2 * multiplier}/${period}`}</span>
                 </div>
             </div>
-            <div className="px-4 flex justify-between">
+            <div className="flex justify-between px-4">
                 <span>total (per month)</span>
-                <span className="text-base lg:text-xl font-bold text-blue-700">
-                    +$12/mo
+                <span className="text-base font-bold text-blue-700 lg:text-xl">
+                    {`+$${12 * multiplier}/${period}`}
                 </span>
             </div>
             <div className="fixed bottom-0  left-0 flex w-full justify-between bg-white p-4 shadow sm:absolute sm:mt-8 sm:p-0 sm:shadow-none md:mb-4">
@@ -60,7 +67,7 @@ const Step4 = () => {
                         e.preventDefault();
                         // router.push('/summary');
                     }}
-                    className="btn rounded-[4px] lg:rounded-lg bg-blue-700 h-10 w-24 lg:w-32 lg:h-12 text-white"
+                    className="btn h-10 w-24 rounded-[4px] bg-blue-700 text-white lg:h-12 lg:w-32 lg:rounded-lg"
                 >
                     Confirm
                 </button>
